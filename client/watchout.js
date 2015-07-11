b875dbb9f99e210b29a95fbc93136a3b91e185ab
Hack Reactor Students <accounts@hackreactor.com>
2015-07-11 12:33:16 -0700

    implement collision detector and update function (score, high score, collisions)

--- client/watchout.js
+++ client/watchout.js
@@ -3,8 +3,14 @@
 var gameOptions = {
   width: 800,
   height: 800,
-  numEnemies: 40
+  numEnemies: 40,
+  radius: 10,
+  score: 0,
+  high: 0,
+  numCollisions: 0
 };
+
+//-------------create player-----------------
 
 var gameBoard = d3.select(".board")
                   .append("svg")
@@ -15,101 +21,120 @@ var gameBoard = d3.select(".board")
 var player = gameBoard.append("circle")
               .attr({
                 id: "player",
-                cx: gameOptions.width/2,
-                cy: gameOptions.height/2,
-                r: 50,
-                fill: "white"
+                cx: gameOptions.width/2 - gameOptions.radius,
+                cy: gameOptions.height/2 - gameOptions.radius,
+                r: gameOptions.radius,
+                fill: "red"
               });
 
-//-------------enemy----------------
+// modified from http://bl.ocks.org/mbostock/1557377
+var drag = d3.behavior.drag()
+    .on("drag", dragmove)
+    .on("dragstart", function(){
+              player.style('opacity', .5);
+            })
+    .on("dragend", function(){
+              player.style('opacity', 1);
+            });
+
+
+function dragmove(d) {
+  d3.select(this)
+      .attr("cx", d3.event.x)
+      .attr("cy", d3.event.y)
+}
+
+var p = d3.select('#player')
+player.call(drag);
+
+//----------------create enemy----------------
 
 
 var enemyData = [];
 
-var createEnemies = function(n){
-
-  // var activeEnemies = [];
-  for(var i = 0 ; i < n; i++){
-    // activeEnemies.push(i);
+var createEnemies = function(n) {
+  for(var i = 0 ; i < n; i++) {
     enemyData.push({
       class: "enemy",
-      x: function() { return Math.random() * gameOptions.width; },
-      y: function() { return Math.random() * gameOptions.width; }
+      x: function() { return Math.random() * 800; },
+      y: function() { return Math.random() * 800; },
     });
   }
 };
 
 createEnemies(gameOptions.numEnemies);
 
-// var createEnemies = function(gameOptions.numEnemies) { 
+var enemies = gameBoard.selectAll(".enemies")
+                       .data(enemyData);
+
+enemies.enter()
+       .append("circle")
+       .attr({
+          class: function(d) { return d.class; },
+          cx: function(d) { return d.x(); },
+          cy: function(d) { return d.y(); },
+          r: gameOptions.radius,
+          fill: "white"
+       });
+
+var moveEnemies = function() {
+  enemies.transition().duration(2000)
+    .attr({
+      cx: function(d) { return d.x(); },
+      cy: function(d) { return d.y(); }, 
+  });
+};
+
+setInterval(moveEnemies, 1000);
+
+//-------------collision----------------
 
 
-// gameBoard.selectAll("circle")
-//           .data(enemyData) //function(e) {return e.id;})
-//           .enter()
-//           .append("circle")
-//           // .attr('class', 'enemy')
-//           .style("stroke", "red")
-//           .style("stroke-width", 2)
-//           .style("fill", "black")
-//           .attr("cx", )
-//           .attr("cy", function() {return Math.random() * gameOptions.height; })
-//           .attr("r", 10);
-// }
+var hasCollision = function() {
+  var collision = false;
+  enemies.each( function(){
+    var pX = p.attr("cx");
+    var pY = p.attr("cy");    
+    var enemyX = d3.select(this).attr("cx");
+    var enemyY = d3.select(this).attr("cy");
+    
+    var xDiff = enemyX - pX;
+    var yDiff = enemyY - pY;
+    var distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 
-// createEnemies(gameOptions.numEnemies);
-
-
-
-
-//   enemies = gameBoard.selectAll('enemy').data(enemyData, function(e){return e.id;});
-
-//   enemies.enter().append('svg:circle').attr('class','enemy')
-//                   .attr('cx',function(e){return e.x;})
-//                   .attr('cy',function(e){return e.y;})
-//                   .attr('r',10);
-
-// };
+    if (distance <= 20) {
+      collision = true;
+      gameOptions.numCollisions++;
+    }
+  });
+  return collision;
+};
 
 
+var update = function() {
+
+  // update current score
+  if (hasCollision()) {
+    gameOptions.score = 0;
+    d3.select(".current > span").text(gameOptions.score);
+  } else {
+    gameOptions.score++;
+    d3.select(".current > span").text(gameOptions.score);
+  }
+
+  // update high score
+  if(gameOptions.high < gameOptions.score) {
+    gameOptions.high = gameOptions.score;
+    d3.select(".high > span").text(gameOptions.high);
+  }
+
+  // update collision
+  d3.select(".collisions > span").text(gameOptions.numCollisions);
+}; 
+
+setInterval(update, 50);
 
 
 
 
 
-
-// d3.select('body').selectAll('div').data([8, 3, 7])
-//     .enter().append('div').style('opacity', 0)
-//     .text('creating divs!')
-//     .transition().duration(3000)
-//     .style('opacity', 1)
-
-// d3.select('body').selectAll('div').data([])
-//     .exit().transition().duration(3000)
-//     .text(function(d){return 'deleting divs '+d})
-//     .style('opacity', 0)
-//     .remove()
-
-// d3.select('body').selectAll('div').data([8, 3, 7, 5])
-//     .text(function(d){return 'existing divs '+d})
-//     .enter().append('div').style('opacity', 0)
-//     .text('creating divs!')
-//     .transition().duration(3000).style('opacity', 1)
-
-// ----------- how to draw circle ----------------------
-// <svg width="50" height="50">
-// 2  <circle cx="25" cy="25" r="25" fill="purple" />
-// 3</svg>
-
-
-// /Make an SVG Container
-//  2var svgContainer = d3.select("body").append("svg")
-//  3                                    .attr("width", 200)
-//  4                                    .attr("height", 200);
-//  5
-//  6//Draw the Ellipse
-//  7var circle = svgContainer.append("circle")
-//  8                         .attr("cx", 50)
-//  9                         .attr("cy", 50)
-// 10                         .attr("rx", 50)
-// 11                         .attr("ry", 50);
