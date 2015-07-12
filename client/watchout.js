940bdcf04eca8472eb13480a0efccecd10ba0582
lisatsewang <lisatsewang@gmail.com>
2015-07-11 17:15:57 -0700

    code clean up and style change

--- client/watchout.js
+++ client/watchout.js
@@ -1,8 +1,8 @@
 // start slingin' some d3 here.
 
 var gameOptions = {
-  width: 800,
-  height: 800,
+  width: 500,
+  height: 500,
   numEnemies: 40,
   radius: 10,
   score: 0,
@@ -13,43 +13,42 @@ var gameOptions = {
 //-------------create player-----------------
 
 var gameBoard = d3.select(".board")
-                  .append("svg")
-                  .attr("width", gameOptions.width)
-                  .attr("height", gameOptions.height)
-                  .append("g")
+  .append("svg")
+  .attr("width", gameOptions.width)
+  .attr("height", gameOptions.height)
+  .append("g");
 
 var player = gameBoard.append("circle")
-              .attr({
-                id: "player",
-                cx: gameOptions.width/2 - gameOptions.radius,
-                cy: gameOptions.height/2 - gameOptions.radius,
-                r: gameOptions.radius,
-                fill: "red"
-              });
+  .attr({
+    id: "player",
+    cx: gameOptions.width/2 - gameOptions.radius,
+    cy: gameOptions.height/2 - gameOptions.radius,
+    r: gameOptions.radius,
+    fill: "red"
+  });
 
 // modified from http://bl.ocks.org/mbostock/1557377
 var drag = d3.behavior.drag()
-    .on("drag", dragmove)
-    .on("dragstart", function(){
-              player.style('opacity', .5);
-            })
-    .on("dragend", function(){
-              player.style('opacity', 1);
-            });
+  .on("drag", dragmove)
+  .on("dragstart", function(){
+            player.style('opacity', .5);
+          })
+  .on("dragend", function(){
+            player.style('opacity', 1);
+          });
 
 function dragmove(d) {
   d3.select(this)
-      .attr("cx", d3.event.x)
-      .attr("cy", d3.event.y);
+    .attr("cx", d3.event.x)
+    .attr("cy", d3.event.y);
 }
 
 function dragmove(d) {
   d3.select(this)
-      .attr("cx", d3.event.x)
-      .attr("cy", d3.event.y)
+    .attr("cx", d3.event.x)
+    .attr("cy", d3.event.y)
 }
 
-var p = d3.select('#player')
 player.call(drag);
 
 //----------------create enemy----------------
@@ -60,8 +59,8 @@ var createEnemies = function(n) {
   for(var i = 0 ; i < n; i++) {
     enemyData.push({
       class: "enemy",
-      x: function() { return Math.random() * 800; },
-      y: function() { return Math.random() * 800; },
+      x: function() { return Math.random() * gameOptions.width; },
+      y: function() { return Math.random() * gameOptions.height; },
     });
   }
 };
@@ -69,17 +68,17 @@ var createEnemies = function(n) {
 createEnemies(gameOptions.numEnemies);
 
 var enemies = gameBoard.selectAll(".enemies")
-                       .data(enemyData);
+  .data(enemyData);
 
-enemies.enter()
-       .append("circle")
-       .attr({
-          class: function(d) { return d.class; },
-          cx: function(d) { return d.x(); },
-          cy: function(d) { return d.y(); },
-          r: gameOptions.radius,
-          fill: "white"
-       });
+  enemies.enter()
+  .append("circle")
+  .attr({
+    class: function(d) { return d.class; },
+    cx: function(d) { return d.x(); },
+    cy: function(d) { return d.y(); },
+    r: gameOptions.radius,
+    fill: "white"
+});
 
 var moveEnemies = function() {
   enemies.transition().duration(2000)
@@ -89,7 +88,7 @@ var moveEnemies = function() {
   });
 };
 
-setInterval(moveEnemies, 1000);
+setInterval(moveEnemies, 2000);
    
 
 //-------------collision----------------
@@ -97,26 +96,32 @@ setInterval(moveEnemies, 1000);
 
 var hasCollision = function() {
   var collision = false;
-  enemies.each( function(){
-    var pX = p.attr("cx");
-    var pY = p.attr("cy");    
+
+  enemies.each( function(d){
+    var pX = player.attr("cx");
+    var pY = player.attr("cy");
+
     var enemyX = d3.select(this).attr("cx");
     var enemyY = d3.select(this).attr("cy");
     
     var xDiff = enemyX - pX;
     var yDiff = enemyY - pY;
+
     var distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 
-    if (distance <= 20) {
+    if (distance <= (gameOptions.radius) * 2) {
       collision = true;
-      gameOptions.numCollisions++;
     }
+    previousCollision = collision;
   });
+
   return collision;
 };
 
 
 var update = function() {
+
+  var previousCollision = false;
 
   // update current score
   if (hasCollision()) {
@@ -134,10 +139,14 @@ var update = function() {
   }
 
   // update collision
-  d3.select(".collisions > span").text(gameOptions.numCollisions);
+  if (hasCollision()) {
+    gameOptions.numCollisions++;
+    d3.select(".collisions > span").text(gameOptions.numCollisions);
+  }
 }; 
 
-setInterval(update, 50);
+// setInterval(update, 50);
+d3.timer(update);
 
 
 
